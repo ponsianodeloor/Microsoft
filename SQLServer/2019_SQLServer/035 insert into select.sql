f45137fadb3ca0a7f0ed04c3f9cdf693d5d0d716
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-06-11 10:26:49 -0500

    035 Insert Into Select varias lineas

--- SQLServer/2019_SQLServer/035 insert into select.sql
+++ SQLServer/2019_SQLServer/035 insert into select.sql
@@ -1,5 +1,6 @@
-INSERT INTO users VALUES(
-'Ponsiano', 'De Loor', 'Masculino', '06-11-2020'
-);
+INSERT INTO users VALUES
+('Jose', 'De Loor', 'Masculino', '05-11-2020'),
+('Tatiana', 'De Loor', 'Masculino', '04-11-2020'),
+('Hilda', 'De Loor', 'Masculino', '03-11-2020');
 
 SELECT * FROM users
