c7e3d62dfcf598787d8f836546d083f2a2c0b2a9
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-06-12 23:25:48 -0500

    051 Respaldo Bd e Insersion en tablas carreras y asignaturas
    Se obtiene un respaldo de la base de datos asi como tambien insersion en la tabla carreras y asignaturas

---
+++ SQLServer/2019_SQLServer/051 Insert Into tabla carreras y asignaturas por carrera_id.sql
@@ -0,0 +1,8 @@
+INSERT INTO asignaturas VALUES
+('Matematica I', 5, 1),
+('Matematica II', 5, 1),
+('Programacion I', 4, 1),
+('Psicologia I', 4, 5),
+('Educativa', 4, 5),
+('Psicologia Emocional', 4, 5),
+('Psicologia del Nino', 4, 5)
